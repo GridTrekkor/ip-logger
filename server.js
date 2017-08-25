0b232f0cfaedfb2d2d95b0b3167ef6284b61b91c
Jonathan Osborne <josborne@itm21st.com>
2017-08-25 09:24:28 -0500

    Add logger

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
 const connect = require('connect');
 const http = require('http');
 const app = connect();
+const winston = require('winston');
+
+winston.configure({
+    transports: [
+        new (winston.transports.File)({ filename: 'ip.log' })
+    ]
+});
 
 // require request-ip and register it as middleware
 const requestIp = require('request-ip');
 app.use(requestIp.mw());
 
 app.use(function(req, res) {
-    // by default, the ip address will be set on the `clientIp` attribute
     const ip = req.clientIp;
+    winston.log('info', ip);
     res.end(ip + '\n');
 });
 
-//create node.js http server and listen on port
+// node http server
 http.createServer(app).listen(7777);
-
-// test it locally from the command line
-// > curl -X GET localhost:3000 # Hello, your ip address is ::1 and is of type IPv6
+console.log('Server running on 7777');
